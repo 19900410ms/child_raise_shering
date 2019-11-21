1f3f25c621255332120064b6ddc35fb1c73c9699
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-21 14:15:47 +0900

    11/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "accepts#index"
+  resources :users, only: [:show, :edit, :update] do
+    resources :comments, only: [:index, :new, :create]
+  end
+  resources :accepts, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :requests, only: [:show, :new, :create, :destroy]
 end
