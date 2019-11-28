46c1d9e3b6276a18d95841aed6bad57a52437636
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-28 14:47:10 +0900

    11/28-1
    bgSwitcher実装

--- app/assets/javascripts/accepts.js
+++ app/assets/javascripts/accepts.js
@@ -1,11 +1,15 @@
-(window).load(function() {
-  $('.offer').bgSwitcher({
-    images: ['assets/images/sample1.jpg', 'assets/images/child2.jpg', 'assets/images/pic3.jpg'], // 切り替える背景画像を指定
-    interval: 400, // 背景画像を切り替える間隔を指定 3000=3秒
+$(window).load(function() {
+  $(".user").bgSwitcher({
+    images: ['http://gahag.net/img/201607/27s/gahag-0110220437-1.jpg',
+             'http://gahag.net/img/201605/20s/gahag-0087939170-1.jpg',
+             'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg',
+             'http://gahag.net/img/201601/22s/gahag-0048425628-1.jpg',
+             'http://gahag.net/img/201608/05s/gahag-0113251319-1.jpg'], // 切り替える背景画像を指定
+    interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
     effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
-    duration: 2000, // エフェクトの時間を指定します。
+    duration: 3500, // エフェクトの時間を指定します。
     easing: "swing" // エフェクトのイージングをlinear,swingから指定
 
   });
