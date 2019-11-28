5ed15d845df644366fdb3de21d7a671bcc1bc7f2
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-28 17:35:08 +0900

    11/28-2
    layoutの微調整

--- app/assets/javascripts/accepts.js
+++ app/assets/javascripts/accepts.js
@@ -1,10 +1,10 @@
 $(window).load(function() {
   $(".user").bgSwitcher({
-    images: ['http://gahag.net/img/201607/27s/gahag-0110220437-1.jpg',
-             'http://gahag.net/img/201605/20s/gahag-0087939170-1.jpg',
-             'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg',
+    images: ['http://gahag.net/img/201608/05s/gahag-0113251319-1.jpg',
+             'http://gahag.net/img/201607/27s/gahag-0110220437-1.jpg',
              'http://gahag.net/img/201601/22s/gahag-0048425628-1.jpg',
-             'http://gahag.net/img/201608/05s/gahag-0113251319-1.jpg'], // 切り替える背景画像を指定
+             'http://gahag.net/img/201605/20s/gahag-0087939170-1.jpg',
+             'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg'], // 切り替える背景画像を指定
     interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
