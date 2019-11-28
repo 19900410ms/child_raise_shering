35c43e588fd698660c8de43aa7eeb4cef30076dd
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-28 19:34:58 +0900

    11/28-3
    Vegas実装

--- app/assets/javascripts/accepts.js
+++ app/assets/javascripts/accepts.js
@@ -5,13 +5,28 @@ $(window).load(function() {
              'http://gahag.net/img/201601/22s/gahag-0048425628-1.jpg',
              'http://gahag.net/img/201605/20s/gahag-0087939170-1.jpg',
              'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg'], // 切り替える背景画像を指定
-    interval: 4000, // 背景画像を切り替える間隔を指定 3000=3秒
+    interval: 3000, // 背景画像を切り替える間隔を指定 3000=3秒
     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
     effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
-    duration: 3500, // エフェクトの時間を指定します。
+    duration: 2500, // エフェクトの時間を指定します。
     easing: "swing" // エフェクトのイージングをlinear,swingから指定
 
   });
 
+  $('.offer').vegas({
+    slides: [
+      { src: "http://gahag.net/img/201608/05s/gahag-0113251319-1.jpg"},
+      { src: "http://farm5.staticflickr.com/4082/4851929570_5b9864d44d.jpg"},
+      { src: "https://find-model.jp/insta-lab/wp-content/uploads/2019/06/kids-baby.jpg"},
+    ],
+    animation: 'kenburns',
+    //overlay: './js/overlays/02.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
+    transition: 'zoomOut', //スライドを遷移させる際のアニメーション
+    transitionDuration: 2500, //スライドの遷移アニメーションの時間
+    delay: 3000, //スライド切り替え時の遅延時間
+    animation: 'random', //スライド表示中のアニメーション
+    animationDuration: 2500, //スライド表示中のアニメーションの時間
+    loop: true,
+  });
 });
