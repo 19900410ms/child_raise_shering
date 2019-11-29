013c30c12b1af576cd7072fc56bdac42a0bd8688
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-29 12:08:29 +0900

    11/29-1
    select_boxの実装
    その他レイアウト微調整

--- app/assets/javascripts/accepts.js
+++ app/assets/javascripts/accepts.js
@@ -4,21 +4,21 @@ $(window).load(function() {
              'http://gahag.net/img/201607/27s/gahag-0110220437-1.jpg',
              'http://gahag.net/img/201601/22s/gahag-0048425628-1.jpg',
              'http://gahag.net/img/201605/20s/gahag-0087939170-1.jpg',
-             'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg'], // 切り替える背景画像を指定
+             'http://gahag.net/img/201608/27s/gahag-0119451581-1.jpg'],
     interval: 3000, // 背景画像を切り替える間隔を指定 3000=3秒
     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
     effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
     duration: 2500, // エフェクトの時間を指定します。
     easing: "swing" // エフェクトのイージングをlinear,swingから指定
-
   });
 
   $('.offer').vegas({
     slides: [
       { src: "http://gahag.net/img/201608/05s/gahag-0113251319-1.jpg"},
-      { src: "http://farm5.staticflickr.com/4082/4851929570_5b9864d44d.jpg"},
       { src: "https://find-model.jp/insta-lab/wp-content/uploads/2019/06/kids-baby.jpg"},
+      { src: "http://farm5.staticflickr.com/4082/4851929570_5b9864d44d.jpg"},
+      { src: "https://www.air-g.co.jp/fcontents/abc//original/program_article_block_image/29/cbd5f8b4753d84968dde3ca57cfd92d7_t.jpg"}
     ],
     animation: 'kenburns',
     //overlay: './js/overlays/02.png', //フォルダ『overlays』の中からオーバーレイのパターン画像を選択
