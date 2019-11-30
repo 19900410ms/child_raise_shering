7ec70b6c2cd982bbe8e0743cbd2e22b6de5a838c
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-11-30 19:54:27 +0900

    11/30-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "accepts#index"
-  resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :comments, only: [:index, :new, :create]
-  end
-  resources :accepts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy]
+  resources :accepts
   resources :requests, only: [:show, :new, :create, :edit, :update, :destroy]
 end
