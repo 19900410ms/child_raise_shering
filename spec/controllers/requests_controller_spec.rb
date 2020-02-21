51c222ea026866d6f351621f7dd003b247f52f51
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-02-21 15:23:11 +0900

    2/21-4
    Request Controller test

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe RequestsController, type: :controller do
+describe RequestsController do
+
+  let(:user)   { create(:user) }
+  
+  describe "#new" do
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      it "renders the :new template" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    context 'not log in' do
+      it 'redirects to new_session_path' do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+  end
 
 end
